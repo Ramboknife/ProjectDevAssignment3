8e986e610e0a2f3aa171365a99b54427f866b106
Ramboknife <60951407+Ramboknife@users.noreply.github.com>
2021-03-12 20:57:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,108 +1,92 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
 <?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
-    include 'logic.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
 
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
-    <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/996973c893.js" crossorigin="anonymous"></script>
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-
-    <!-- My Stylesheet -->
-    <link rel="stylesheet" href="style.css">
-
-    <!-- My jQuery -->
-    <script src="main.js"></script>
-
-    <!-- Chart.js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-
-    <title>Covid-19 Tracker</title>
-</head>
-<body>
-    <div class="container-fluid bg-light p-5 text-center my-3">
-        <h1 class="">Covid-19 Tracker</h1>
-        <h5 class="text-muted">An opensource project to keep track of all the COVID-19 cases around the world.</h5>
-    </div>
-
-    <div class="container my-5">
-        <div class="row text-center">
-            <div class="col-4 text-warning">
-                <h5>Confirmed</h5>
-                <?php echo $total_confirmed;?>
-            </div>
-            <div class="col-4 text-success">
-                <h5>Recovered</h5>
-                <?php echo $total_recovered;?>
-            </div>
-            <div class="col-4 text-danger">
-                <h5>Deceased</h5>
-                <?php echo $total_deaths;?>
-            </div>
-        </div>
-    </div>
-
-    <div class="container bg-light p-3 my-5 text-center">
-        <h5 class="text-info">"Prevention is the Cure."</h5>
-        <p class="text-muted">Stay Indoors Stay Safe.</p>
-    </div>
-
-    <canvas id="myChart"></canvas>
-
-    <div class="container-fluid">
-        <div class="table-responsive">
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Countries</th>
-                        <th scope="col">Confirmed</th>
-                        <th scope="col">Recovered</th>
-                        <th scope="col">Deceased</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach($data as $key => $value){
-                            $increase = $value[$days_count]['confirmed'] - $value[$days_count_prev]['confirmed'];
-                    ?>
-                        <tr>
-                            <th scope="row"><?php echo $key?></th>
-                            <td>
-                                <?php echo $value[$days_count]['confirmed'];?>
-                                <?php if($increase != 0){ ?>
-                                    <small class="text-danger pl-3"><i class="fas fa-arrow-up"></i><?php echo $increase;?></small>  
-                                <?php } ?>    
-                            </td>
-                            <td><?php echo $value[$days_count]['recovered'];?></td>
-                            <td><?php echo $value[$days_count]['deaths'];?></td>
-                        </tr>
-                    <?php }?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-    <footer class="footer mt-auto py-3 bg-light">
-        <div class="container text-center">
-            <span class="text-muted">Copyright &copy;2020, <a href="https://packetcode.in" target="_blank">Packetcode</a></span>
-        </div>
-    </footer>
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
 
 </body>
 </html>
