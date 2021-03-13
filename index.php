62e39db70415908eee828be6d5da758e246ca01e
Ramboknife <60951407+Ramboknife@users.noreply.github.com>
2021-03-12 21:04:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ function test_input($data) {
   <input type="radio" name="gender" value="Female">Female
   <input type="radio" name="gender" value="Male">Male
   <input type="radio" name="gender" value="Other">Other
-  <span class="error">* <?php echo $genderErr;?></span>
+  <span class="error"><?php echo $genderErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
